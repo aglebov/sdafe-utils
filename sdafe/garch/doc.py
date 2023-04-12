66923b36e541451fbfcdbe1d25f5c6a314a452d2
Artem Glebov <artem.glebov@gmail.com>
2023-04-12 17:33:20 +0100

    Function to test for dynamic orthogonal components

---
+++ sdafe/garch/doc.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+
+def doc_test(A: np.array, m: int) -> pd.DataFrame:
+    """Test for dynamic orthogonal components
+
+    Parameters
+    ----------
+    A: np.array
+        the matrix of time series: rows are observations, columns are variables
+    m: int
+        the number of lags to test
+
+    Returns
+    -------
+    pd.DataFrame
+        a dataframe with three columns:
+
+           - `Q(m)`: the test statistic
+           - `d.f.`: the number of degrees of freedom
+           - `p-value`: the p-value
+    """
+    n, k = A.shape
+    res = []
+
+    q = n * np.sum(np.tril(np.corrcoef(A, rowvar=False), k=-1) ** 2)
+    df = k * (k - 1) / 2  # lower triangular excluding the diagonal
+    p = 1 - stats.chi2.cdf(q, df=df)
+    res.append((q, df, p))
+
+    for j in range(1, m + 1):
+        # corrcoef gives us correlation between all combinations of variables in x and y,
+        # but we only need the correlations of variables in x with variables in y,
+        # hence select a submatrix
+        ccf = np.corrcoef(A[j:, :], A[:-j, :], rowvar=False)[:k, k:]
+        q += n * (n + 2) * np.sum((ccf - np.diag(np.diag(ccf))) ** 2) / (n - j)  # zero the diagonal elements
+        df += k * (k - 1)  # excluding the diagonal elements
+        p = 1 - stats.chi2.cdf(q, df=df)
+        res.append((q, df, p))
+
+    return pd.DataFrame(res, columns=['Q(m)', 'd.f.', 'p-value'], index=pd.Index(range(m + 1), name='m'))
