73dcee220add676795c19645bd46ea144cb62a54
Artem Glebov <artem.glebov@gmail.com>
2023-04-21 19:26:27 +0100

    Function to replicate R's stats::princomp

---
+++ sdafe/ch18/pca.py
@@ -0,0 +1,83 @@
+from typing import Union
+
+import numpy as np
+import pandas as pd
+
+
+class PCAResult:
+    """Summary of results of a PCA"""
+
+    def __init__(self, n_obs: int, center: np.array, sdev: np.array, loadings: np.array,
+                 scores: np.array):
+        """Initialise PCA results
+
+        Parameters
+        ----------
+        n_obs: int
+            the number of observations in the input data
+        center: np.array
+            the means of the input variables
+        sdev: np.array
+            the standard deviations of the principal components
+            (the square roots of the eigenvalues)
+        loadings: np.array
+            the component loadings (the eigenvectors)
+        scores: np.array
+            the projections of input data on the principal components
+        """
+        self.n_obs = n_obs
+        self.center = center
+        self.sdev = sdev
+        self.loadings = loadings
+        self.scores = scores
+
+    def summary(self) -> pd.DataFrame:
+        """Returns a component importance summary"""
+        var = self.sdev ** 2
+        var_proportion = var / np.sum(var)
+        return pd.DataFrame([
+            self.sdev,
+            var_proportion,
+            np.cumsum(var_proportion),
+        ], columns=[f'Comp.{i + 1}' for i in range(len(self.sdev))],
+            index=['Standard deviation', 'Proportion of Variance', 'Cumulative Proportion'])
+
+
+def princomp(X: Union[np.array, pd.DataFrame], cor: bool = False, compat: bool = True):
+    """Performs a PCA and returns a summary of the results
+
+    Parameters
+    ----------
+    X: Union[np.array, pd.DataFrame]
+        the data to use: rows are observations, columns are variables
+    cor: bool
+        if True, perform a PCA of the correlation matrix of the data,
+        otherwise of the covariance matrix. Default: False
+    compat: bool
+        if True, use the population rather than sample covariance/correlation
+        estimate for compatibility with princomp in R
+
+    Returns
+    -------
+    PCAResult
+        a summary of the results of PCA
+    """
+    means = np.mean(X, axis=0)
+    X = X - means
+    if cor:
+        cov = np.corrcoef(X, rowvar=False, ddof=1 if not compat else 0)
+    else:
+        cov = np.cov(X, rowvar=False, ddof=1 if not compat else 0)
+
+    w, v = np.linalg.eig(cov)
+    order = np.flip(np.argsort(w))
+    w = w[order]
+    v = v[:, order]
+
+    return PCAResult(
+        n_obs=X.shape[0],
+        center=means,
+        sdev=np.sqrt(w),
+        loadings=v,
+        scores=X @ v,
+    )
