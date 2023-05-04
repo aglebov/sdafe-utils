2204c3ed4cc036f0cce986e51adb2f498d25aa56
Artem Glebov <artem.glebov@gmail.com>
2023-05-04 13:59:23 +0100

    Expose loglik_mvt in ch07.multivariate

--- sdafe/ch07/multivariate.py
+++ sdafe/ch07/multivariate.py
@@ -6,6 +6,44 @@ from scipy.optimize import minimize, OptimizeResult
 import scipy.stats as stats
 
 
+def loglik_mvt(Y: pd.DataFrame, par: np.ndarray) -> float:
+    """The log-likelihood of the multivariate normal distribution
+
+    Parameters
+    ----------
+    Y: pd.DataFrame
+        the input data: rows are observations, columns are variables
+    par: np.ndarray
+        the parameters of the multivariate t-distribution
+
+    Returns
+    -------
+    float
+        the log-likelihood of the multivariate t-distribution evaluated at the given point
+
+
+    Notes
+    -----
+    The parameters are passed as a one-dimensional array: first the means,
+    then the elements of the upper-triangular matrix A, where A.T @ T ~ Cov(Y),
+    followed by the degrees of freedom.
+    """
+    n = Y.shape[1]  # the number of variables
+    n_mu = n
+    n_cov = n * (n + 1) // 2  # upper-triangular elements of an n-by-n matrix
+
+    # the means of the multivariate t are in the first `n_mu` elements
+    mu = par[:n_mu]
+    # the upper triangular elements of A are in the following `n_cov` elements
+    A = np.zeros((n, n))
+    A[np.triu_indices(n)] = par[n_mu:n_mu + n_cov]
+    # the scale matrix
+    scale = A.T @ A
+    # the degrees of freedom value is the last element of `par`
+    df = par[n_mu + n_cov]
+    return -np.sum(np.log(stats.multivariate_t.pdf(Y, loc=mu, shape=scale, df=df)))
+
+
 def fit_mvt(
         Y: pd.DataFrame,
         mu_bounds: Tuple[float, float],
@@ -34,18 +72,6 @@ def fit_mvt(
     n_mu = n
     n_cov = n * (n + 1) // 2  # upper-triangular elements of an n-by-n matrix
 
-    def loglik_f(par):
-        # the means of the multivariate t are in the first `n_mu` elements
-        mu = par[:n_mu]
-        # the upper triangular elements of A are in the following `n_cov` elements
-        A = np.zeros((n, n))
-        A[np.triu_indices(n)] = par[n_mu:n_mu + n_cov]
-        # the scale matrix
-        scale = A.T @ A
-        # the degrees of freedom value is the last element of `par`
-        df = par[n_mu + n_cov]
-        return -np.sum(np.log(stats.multivariate_t.pdf(Y, loc=mu, shape=scale, df=df)))
-
     # the starting values for the search
     A = np.linalg.cholesky(Y.cov()).T
     start = np.concatenate([Y.mean().values, A[np.triu_indices(n)], [4]])
@@ -54,4 +80,9 @@ def fit_mvt(
     lower = np.array([mu_bounds[0]] * n_mu + [a_bounds[0]] * n_cov + [df_bounds[0]])
     upper = np.array([mu_bounds[1]] * n_mu + [a_bounds[1]] * n_cov + [df_bounds[1]])
 
-    return minimize(loglik_f, start, method='L-BFGS-B', bounds=list(zip(lower, upper)))
+    def objective_func(beta):
+        return loglik_mvt(Y, beta)
+
+    bounds = list(zip(lower, upper))
+
+    return minimize(objective_func, start, method='L-BFGS-B', bounds=bounds)
