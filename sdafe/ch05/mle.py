b4273f1829392f84ce76c5cabc0ededc13a25934
Artem Glebov <artem.glebov@gmail.com>
2023-05-17 17:49:41 +0100

    AIC and BIC estimates based on the results of an MLE fit

--- sdafe/ch05/mle.py
+++ sdafe/ch05/mle.py
@@ -1,6 +1,7 @@
 from typing import Any, Callable, Optional
 
 import numpy as np
+import scipy.optimize
 from statsmodels.tools.numdiff import approx_hess1
 
 
@@ -22,3 +23,37 @@ def mle_se(x: Any, loglik_f: Callable[[Any], float], epsilon: Optional[float] = 
         the array of standard errors
     """
     return np.sqrt(np.diag(np.linalg.inv(approx_hess1(x, loglik_f, epsilon=epsilon))))
+
+
+def aic(fit: scipy.optimize.OptimizeResult) -> float:
+    """AIC estimate based on the results of an MLE fit
+
+    Parameters
+    ----------
+    fit: scipy.optimize.OptimizeResult
+        the result of minimizing the negative log-likelihood
+
+    Returns
+    -------
+    float
+        the AIC estimate of the fit
+    """
+    return 2 * fit.fun + 2 * len(fit.x)
+
+
+def bic(fit: scipy.optimize.OptimizeResult, n: int) -> float:
+    """BIC estimate based on the results of an MLE fit
+
+    Parameters
+    ----------
+    fit: scipy.optimize.OptimizeResult
+        the result of minimizing the negative log-likelihood
+    n: int
+        the number of observations used in the fit
+
+    Returns
+    -------
+    float
+        the BIC estimate of the fit
+    """
+    return 2 * fit.fun + np.log(n) * len(fit.x)
